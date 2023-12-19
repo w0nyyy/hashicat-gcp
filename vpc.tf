3fc05da0819d888d0759737bfed3adb290891aff
wony <you@example.com>
2023-12-19 06:24:40 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -2,12 +2,13 @@ module "network" {
   source  = "app.terraform.io/test-redy/network/google"
   version = "3.4.0"
   # insert required variables here
-}
 
-subnets = [
+  subnets = [
   {
     subnet_name   = "gaurav-subnet"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
   }
 ]
+}
+
